dc58e7c93cfd786c6a56f3e618e96a70e02f826b
Kaleb Ball <kaleb.ball@gmail.com>
2020-10-05 13:41:01 -0500

    Important changes

--- mavit-client/src/app/app.module.ts
+++ mavit-client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './components/hello-world/hello-world.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HelloWorldComponent } from './components/hello-world/hello-world.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
