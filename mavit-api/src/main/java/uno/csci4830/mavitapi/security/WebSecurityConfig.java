96651556b5636438695ea1341dd275879e1d8ce3
Kaleb Ball <kaleb.ball@gmail.com>
2020-10-29 13:51:16 -0500

    Fixes for production security

--- mavit-api/src/main/java/uno/csci4830/mavitapi/security/WebSecurityConfig.java
+++ mavit-api/src/main/java/uno/csci4830/mavitapi/security/WebSecurityConfig.java
@@ -56,6 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/", "", "/login", "/signup").permitAll()
                 .antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
 
