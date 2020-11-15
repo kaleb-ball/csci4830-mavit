a6db31f0a04dc62c6b99db95c8887e74ef77e85f
Riley Kilzer <rkilzer@unomaha.edu>
2020-11-15 17:28:52 -0600

    Removed unnecessary code

--- mavit-client/src/app/app-routing.module.ts
+++ mavit-client/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { CommentComponent } from './components/comments/comment.component';
 
 const routes: Routes = [
   {path : 'login', component : LoginComponent},
   {path : 'signup', component : SignupComponent},
-  {path : 'comment', component : CommentComponent}
 ];
 
 @NgModule({
