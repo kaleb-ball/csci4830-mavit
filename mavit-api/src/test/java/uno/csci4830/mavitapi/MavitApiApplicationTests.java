b3920e661420d7a4a141c169dd3306af3e4edd27
Riley Kilzer <rkilzer@unomaha.edu>
2020-12-06 21:34:56 -0600

    Basic Test Cases

--- mavit-api/src/test/java/uno/csci4830/mavitapi/MavitApiApplicationTests.java
+++ mavit-api/src/test/java/uno/csci4830/mavitapi/MavitApiApplicationTests.java
@@ -1,13 +1,68 @@
 package uno.csci4830.mavitapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import uno.csci4830.mavitapi.controller.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MavitApiApplicationTests {
 
+	@Autowired
+	private AuthenticationController authController;
+
+	@Autowired
+	private CommentController commentController;
+
+	@Autowired
+	private CommonController commonController;
+
+	@Autowired
+	private HomeController homeController;
+
+	@Autowired
+	private PageController pageController;
+
+	@Autowired
+	private TestController testController;
+
+	@Autowired
+	private ThreadController threadController;
+
 	@Test
-	void contextLoads() {
+	void contextLoadsAuth() {
+		assertThat(authController).isNotNull();
 	}
 
+	@Test
+	void contextLoadsComment() {
+		assertThat(commentController).isNotNull();
+	}
+
+	@Test
+	void contextLoadsCommon() {
+		assertThat(commonController).isNotNull();
+	}
+
+	@Test
+	void contextLoadsHome() {
+		assertThat(homeController).isNotNull();
+	}
+
+	@Test
+	void contextLoadsPage() {
+		assertThat(pageController).isNotNull();
+	}
+
+	@Test
+	void contextLoadsTest() {
+		assertThat(testController).isNotNull();
+	}
+
+	@Test
+	void contextLoadsThread() {
+		assertThat(threadController).isNotNull();
+	}
 }
